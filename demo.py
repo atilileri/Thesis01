dda961673bbf469f7354ccb5195856e45a845e14
atilileri <atil@live.it>
2018-07-16 01:14:39 +0300

    dimensionality problem here

--- demo.py
+++ demo.py
@@ -37,6 +37,8 @@ This length is used throughout the algorithm as the frame length (see Section II
 for fileInfo in readExampleSet(path):
     # print(fileInfo)
     # print(len(fileInfo[2]))
+    subframeDurationBySample = int(subframeDuration * fileInfo[1])
+    allMatrixForFile = list()
 
     # todo : ask if lfilter() works true?
     ''' Step B.2:
@@ -44,7 +46,6 @@ for fileInfo in readExampleSet(path):
     Each subframe is then pre-emphasized using a first-order difference filter ( H(z) = 1 - alpha * z^-1 
     where alpha = 0.95)
     '''
-    subframeDurationBySample = int(subframeDuration * fileInfo[1])
     for i in range(0, len(fileInfo[2]), int(hopSize * fileInfo[1])):
         # Index to stop: At the end of the subframe, index must stop before the file ends.
         # This assignment adjusts the last step size according to info above.
@@ -53,30 +54,32 @@ for fileInfo in readExampleSet(path):
         # print(stopIdx)
         signal.lfilter([1, -1*alpha], -1, fileInfo[2][i:stopIdx])
 
-    ''' Step B.3:
-    For each breath example, the MFCC are computed for every subframe, thus forming a short-time cepstrogram
-    representation of the example. The cepstrogram is defined as a matrix whose columns are the MFCC vectors
-    for each subframe. Each such matrix is denoted by M(i), i=1,2,...,N where N is the number of examples in the
-    examples set. The construction of the cepstrogram is demonstrated in Fig. 4.
-    '''
-    # http://python-speech-features.readthedocs.io/en/latest/#python_speech_features.base.mfcc
-    mfccMatrix = python_speech_features.mfcc(signal=fileInfo[2],
-                                             samplerate=fileInfo[1],
-                                             winlen=subframeDuration,
-                                             winstep=hopSize)
-    # print(len(mfccMatrix))
-    # print(mfccMatrix)
+        ''' Step B.3:
+        For each breath example, the MFCC are computed for every subframe, thus forming a short-time cepstrogram
+        representation of the example. The cepstrogram is defined as a matrix whose columns are the MFCC vectors
+        for each subframe. Each such matrix is denoted by M(i), i=1,2,...,N where N is the number of examples in the
+        examples set. The construction of the cepstrogram is demonstrated in Fig. 4.
+        '''
+        # http://python-speech-features.readthedocs.io/en/latest/#python_speech_features.base.mfcc
+        mfccMatrix = list()
+        mfccMatrix.append(python_speech_features.mfcc(signal=fileInfo[2][i:stopIdx],
+                                                      samplerate=fileInfo[1],
+                                                      winlen=subframeDuration,
+                                                      winstep=hopSize))
+        # print(len(mfccMatrix))
+        # print(mfccMatrix)
 
-    ''' Step B.4:
-    For each column of the cepstrogram, DC removal is performed, resulting in the matrix M(i) i=1,2,...,N.
-    '''
-    for column in mfccMatrix:
-        # print(np.mean(column))
-        # print(column)
-        column = column - np.mean(column)
-        # print(column)
+        ''' Step B.4:
+        For each column of the cepstrogram, DC removal is performed, resulting in the matrix M(i) i=1,2,...,N.
+        '''
+        for column in mfccMatrix:
+            # print(np.mean(column))
+            # print(column)
+            column = column - np.mean(column)
+            # print(column)
 
-    # print(mfccMatrix)
+        # print(mfccMatrix)
+        allMatrixForFile.append(mfccMatrix)
 
     ''' Step B.5:
     A mean cepstrogram is computed by averaging the matrices of the example set.
@@ -84,4 +87,8 @@ for fileInfo in readExampleSet(path):
     This defines the template matrix T. In a similar manner, a variance matrix V is computed, where the distribution of
     each coefficient is measured along the example set.
     '''
-    #  todo : implement here. Variance Matrix search ?
+    # todo : resolve dimension problem
+    # print(allMatrixForFile)
+    # templateMatrix = np.mean(allMatrixForFile)
+
+    #  todo : implement here. Variance Matrix.
