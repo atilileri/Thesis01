f60648d6500b728333034c4994d648e7e403fbd7
atilileri <atil@live.it>
2018-07-26 01:27:34 +0300

    added wave files. utils created. detection phase started

--- instFreqFromScipyHilbert.py
+++ instFreqFromScipyHilbert.py
@@ -4,16 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import hilbert, chirp
 # atili: addition imports
-import scipy.io.wavfile
-
-signalType = 1
+import utils
 
 # In this example we use the Hilbert transform to determine the amplitude envelope and instantaneous frequency
 # of an amplitude-modulated signal.
 
 # Note that End-point limitation exists in the example
 
-if 0 == signalType:
+if 0 == utils.signalType:
     duration = 1.0
     fs = 400.0
     samples = int(fs*duration)
@@ -22,49 +20,50 @@ if 0 == signalType:
 
     signal = chirp(t, 20.0, t[-1], 100.0)
     signal *= (1.0 + 0.5 * np.sin(2.0*np.pi*3.0*t))
-elif 1 == signalType:
+elif 1 == utils.signalType:
     '''
     START - atili additions
     '''
 
-    # my local copies
-    # path = 'D:/atili/MMIExt/Audacity/METU Recordings/hh2_breath/hh2_09_01.20.741_134_en.wav'
-    # path = 'D:/atili/MMIExt/Audacity/METU Recordings/hh2_breath/hh2_09_01.20.741_134_en3_16bit.wav'
-    # path = './Initial Breath Examples/bb_tr001_cigdem_07.wav'
+    path = '/METU Recordings/hh2_breath/hh2_09_01.20.741_134_en.wav'
+    # path = '/METU Recordings/hh2_breath/hh2_09_01.20.741_134_en3_16bit.wav'
+    # path = '/METU Recordings/Initial Breath Examples/bb_tr001_cigdem_07.wav'
 
-    # github copies
-    path = './hh2_breath/hh2_09_01.20.741_134_en.wav'
-    # path = './hh2_breath/hh2_09_01.20.741_134_en3_16bit.wav'
-    # path = './Initial Breath Examples/bb_tr001_cigdem_07.wav'
+    path = utils.prefix + path
+    fs, signal = utils.readMonoWav(path)
 
-    fs, signal = scipy.io.wavfile.read(path)
-    print(fs, signal, signal.dtype)
+    print(fs, 'Hz, Size=', signal.dtype, '*', len(signal), 'bytes, Array:', signal)
     t = np.arange(len(signal)) / fs
     '''
     END - atili additions
     '''
 else:
     print('TYPE NOT DEFINED')
+    signal = None
+    fs = None
+    t = None
 
-# The amplitude envelope is given by magnitude of the analytic signal. The instantaneous frequency can be obtained by
-#  differentiating the instantaneous phase in respect to time. The instantaneous phase corresponds to the phase angle
-#  of the analytic signal.
 
-analytic_signal = hilbert(signal)
-amplitude_envelope = np.abs(analytic_signal)
-instantaneous_phase = np.unwrap(np.angle(analytic_signal))
-instantaneous_frequency = (np.diff(instantaneous_phase) /
-                           (2.0*np.pi) * fs)
+if signal is not None:
+    # The amplitude envelope is given by magnitude of the analytic signal. The instantaneous frequency can be obtained
+    # by differentiating the instantaneous phase in respect to time. The instantaneous phase corresponds to the phase
+    # angle of the analytic signal.
 
-fig = plt.figure()
-ax0 = fig.add_subplot(211)
-ax0.plot(t, signal, label='signal')
-ax0.plot(t, amplitude_envelope, label='envelope')
-ax0.set_xlabel("time in seconds")
-ax0.legend()
-ax1 = fig.add_subplot(212)
-ax1.plot(t[1:], instantaneous_frequency)
-ax1.set_xlabel("time in seconds")
-# ax1.set_ylim(0.0, 120.0)
-plt.show()
+    analytic_signal = hilbert(signal)
+    amplitude_envelope = np.abs(analytic_signal)
+    instantaneous_phase = np.unwrap(np.angle(analytic_signal))
+    instantaneous_frequency = (np.diff(instantaneous_phase) /
+                               (2.0*np.pi) * fs)
+
+    fig = plt.figure()
+    ax0 = fig.add_subplot(211)
+    ax0.plot(t, signal, label='signal')
+    ax0.plot(t, amplitude_envelope, label='envelope')
+    ax0.set_xlabel("time in seconds")
+    ax0.legend()
+    ax1 = fig.add_subplot(212)
+    ax1.plot(t[1:], instantaneous_frequency)
+    ax1.set_xlabel("time in seconds")
+    # ax1.set_ylim(0.0, 120.0)
+    plt.show()
 
---
+++ utils.py
@@ -0,0 +1,123 @@
+"""
+utils for common use
+"""
+import scipy.io.wavfile
+import numpy as np
+from scipy import signal
+import python_speech_features
+import os
+import glob
+
+'''
+Project Configuration
+'''
+# for hht versions
+signalType = 1  # 0 for original, 1 for our signal
+
+# for sound file sources
+insideProject = True  # if sound files are taken from inside the project folder
+if insideProject:
+    prefix = '.'
+else:
+    prefix = 'D:/atili/MMIExt/Audacity'
+
+
+# function to read wav files, assuring that resulting signal is mono channel.
+def readMonoWav(pathToFile):
+    samplingRate, samples = scipy.io.wavfile.read(pathToFile)
+    # stereo to mono
+    if len(samples.shape) > 1 and 1 < samples.shape[1]:  # python evals expressions LTR so it's safe to write this
+        # todo - hh: maybe all channels can be used as different inputs to augment the data?
+        samplesMono = np.mean(samples, axis=1, dtype=samples.dtype)
+    else:
+        samplesMono = samples
+
+    return samplingRate, samplesMono
+
+
+# reads wav files in the given folder
+def readExampleSet(folderPath):
+    exampleSet = []
+    minlen = 999999  # pseudo big number
+    '''
+    Step II-A.1:
+    Several signals containing isolated breath examples are selected, forming the example set. From each example, a section
+    of fixed length, typically equal to the length of the shortest example in the set (about 100–160 ms), is derived.
+    This length is used throughout the algorithm as the frame length (see Section II-C).
+    '''
+    for filePath in glob.glob(os.path.join(folderPath, '*.wav')):
+        sampleRate, samples = readMonoWav(filePath)
+
+        exampleSet.append([filePath, sampleRate, samples])
+        if len(samples) < minlen:
+            minlen = len(samples)
+            # todo - ai: minlen diff looks huge, create better samples
+            # print('min frame:', minlen)
+
+    # fix frame length. Refer to Step B.1
+    for file in exampleSet:
+        file[2] = file[2][:minlen]
+
+    return exampleSet, minlen/sampleRate
+
+
+# function to create mfcc  matrix of a audio file
+def createMfccMatrix(sig,
+                     sampRate,
+                     subframeDuration=0.010,  # 10 ms
+                     hopSize=0.005):  # 5 ms
+    subframeDurationBySample = int(subframeDuration * sampRate)
+    alpha = 0.95
+
+    # todo - hh: ask if lfilter() works true?
+    ''' Step II-A.2:
+    Each breath example is divided into short consecutive subframes, with duration of 10 ms and hop size of 5 ms.
+    Each subframe is then pre-emphasized using a first-order difference filter ( H(z) = 1 - alpha * z^-1 
+    where alpha = 0.95)
+    '''
+    mfccMatrix = list()
+    for i in range(0, len(sig), int(hopSize * sampRate)):
+        # Index to stop: At the end of the subframe, index must stop before the file ends.
+        # This assignment adjusts the last step size according to info above.
+        stopIdx = min(i + subframeDurationBySample, len(sig) - 1)
+        # print(i)
+        # print(stopIdx)
+        emphasized = signal.lfilter([1, -1 * alpha], -1, sig[i:stopIdx])
+
+        ''' Step II-A.3:
+        For each breath example, the MFCC are computed for every subframe, thus forming a short-time cepstrogram
+        representation of the example. The cepstrogram is defined as a matrix whose columns are the MFCC vectors
+        for each subframe. Each such matrix is denoted by M(i), i=1,2,...,N where N is the number of examples in the
+        examples set. The construction of the cepstrogram is demonstrated in Fig. 4.
+        '''
+        # http://python-speech-features.readthedocs.io/en/latest/#python_speech_features.base.mfcc
+        # mfcc() function is designed to work on longer signals and divide them into subframes using winlen and winstep
+        # parameters. What we do instead is, we split the sgnal ourselves into subframes, apply the filter on the
+        # subframe and put it into mfcc() function. So we get 1 mfcc row for each subframe. Because function is designed
+        # to work on longer signals, it returns 2d array, each row holding 1 mfcc vector. Since we only get 1 vector,
+        # our result is 2d array with the shape of (1,13). So we only get 1st row, as 1d array of 13 elements.
+        mfccMatrix.append(python_speech_features.mfcc(signal=emphasized,
+                                                      samplerate=sampRate,
+                                                      winlen=subframeDuration,
+                                                      winstep=hopSize)[0])  # read above explanation for '[0]' index.
+        # todo - hh: ask above design choice and/or implementation is true. we can apply the filter on the whole signal
+        # todo - hh: and run mfcc() on the whole signal. Instead we do subframing first and then doing both on same time
+        # print(len(mfccMatrix))
+        # print(mfccMatrix)
+
+    ''' Step II-A.4:
+    For each column of the cepstrogram, DC removal is performed, resulting in the matrix M(i) i=1,2,...,N.
+    '''
+    # print('Before:', mfccMatrix)
+    for i in range(len(mfccMatrix)):
+        # print(np.mean(mfccMatrix[i]))
+        # print(mfccMatrix[i].shape)
+        mfccMatrix[i] = mfccMatrix[i] - np.mean(mfccMatrix[i])
+        # print(mfccMatrix[i])
+
+    # print('After:', mfccMatrix)
+    return mfccMatrix
+
+
+def calcShortTimeEnergy(sig):
+    return sig
