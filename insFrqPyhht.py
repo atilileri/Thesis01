1c0fa5071ce10883e063abae55ff4568865c46d5
atilileri <atil@live.it>
2018-12-17 04:06:28 +0300

    Converting signal into an analytic one using hilbert(), according to: https://github.com/jaidevd/pyhht/issues/43#issuecomment-398077924

--- insFrqPyhht.py
+++ insFrqPyhht.py
@@ -8,23 +8,33 @@ from tftb.generators import fmsin
 import matplotlib.pyplot as plt
 from utils import readMonoWav
 import numpy
+from scipy.signal import hilbert
 numpy.set_printoptions(edgeitems=50)
 
-x = readMonoWav('./METU Recordings/hh2_48kHz_Mono_32bitFloat.wav')[1]
-print(x, len(x))
+sig = readMonoWav('./METU Recordings/hh2_48kHz_Mono_32bitFloat.wav')[1]
+print(sig, len(sig))
 print('##########################################################################')
-# print(type(x[1]))
+# print(type(sig[1]))
 # x = fmsin(70, 0.05, 0.35, 25)[0]
-# print(type(x))
-instf, timestamps = inst_freq(x)
+# print(type(sig))
+
+# take a sample for better visualising, huge chunks crashes in hilbert()
+sig = sig[4000000:4001000]
+
+# Changes according to
+# https://github.com/jaidevd/pyhht/issues/43#issuecomment-398077924
+sig = hilbert(sig)
+
+
+instf, timestamps = inst_freq(sig)
 print(instf, len(instf))
 
 # todo - atili: try more with whole_speech and hh2 wave files
-'''
+
 plt.plot(timestamps, instf)
 plt.grid(True, linestyle='dotted')
 plt.xlabel('Time')
 plt.ylabel('Frequency')
 plt.title('Instantaneous frequency estimation')
 plt.show()
-'''
+
