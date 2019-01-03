c3c770fcc2c5f6b96e118ab53eed79e02995c6ca
atilileri <atil@live.it>
2019-01-04 01:28:18 +0300

    emd and imf extraction added

--- insFrqPyhht.py
+++ insFrqPyhht.py
@@ -3,69 +3,65 @@ Code taken from:
 https://pyhht.readthedocs.io/en/latest/apiref/pyhht.html#pyhht.utils.inst_freq
 [2018.08.30]: Tried, works as example
 """
+from pyhht.emd import EMD
+from pyhht.visualization import plot_imfs
 from tftb.processing import inst_freq
-from tftb.generators import fmsin
-import matplotlib.pyplot as plt
 from utils import readMonoWav
-import numpy
 from scipy.signal import hilbert
+import matplotlib.pyplot as plt
+import numpy
 numpy.set_printoptions(edgeitems=50)
 
-# sig = readMonoWav('./METU Recordings/hh2_48kHz_Mono_32bitFloat.wav')[1]
-# sig = readMonoWav('./METU Recordings/hh2_breath/hh2_04_00.34.164_270_en.wav')[1]
-# sig = readMonoWav('./METU Recordings/hh2_breath/hh2_09_01.20.741_134_en2.wav')[1]
-# sig = readMonoWav('./METU Recordings/hh2_breath/hh2_09_01.20.741_134_en3_16bit.wav')[1]
-# sig = readMonoWav('./METU Recordings/hh2_breath/hh2_23_03.02.050_149_tr.wav')[1]
-sig = readMonoWav('./METU Recordings/hh2_withEdges/hh2_random001.wav')[1]
+# sampRate, sig = readMonoWav('./METU Recordings/hh2_48kHz_Mono_32bitFloat.wav')
+# sampRate, sig = readMonoWav('./METU Recordings/hh2_breath/hh2_04_00.34.164_270_en.wav')
+# sampRate, sig = readMonoWav('./METU Recordings/hh2_breath/hh2_09_01.20.741_134_en2.wav')
+# sampRate, sig = readMonoWav('./METU Recordings/hh2_breath/hh2_09_01.20.741_134_en3_16bit.wav')
+# sampRate, sig = readMonoWav('./METU Recordings/hh2_breath/hh2_23_03.02.050_149_tr.wav')
+sampRate, sig = readMonoWav('./METU Recordings/hh2_withEdges/hh2_random001.wav')
 
-print(sig, len(sig))
-print('##########################################################################')
-# print(type(sig[1]))
-# x = fmsin(70, 0.05, 0.35, 25)[0]
-# print(type(sig))
+# print(sig)
+print(len(sig))
+print(sampRate)
+print('######')
 
-readWholeFile = True
+readWholeFile = True  # for trials only
 
-if readWholeFile:  # for tests if file is readable. Does not visualize well and plotting takes too long
-    # todo - hh: may be we have to calculate average on small chunks?
-    timestamps = []
-    instf = []
-
-    for i in range(len(sig)//1000):
-        part = sig[i*1000:(i+1)*1000]
-        # Converting signal into an analytic one using hilbert(), according to:
-        # https://github.com/jaidevd/pyhht/issues/43#issuecomment-398077924
-        part = hilbert(part)
-        partInstf, partTimestamps = inst_freq(part)
-        instf.extend(partInstf)
-        timestamps.extend(numpy.add(partTimestamps, i*1000))
-        print(i)
-
-else:
+if readWholeFile is False:
     # take a sample for better visualising, huge chunks crashes in hilbert()
-    sig = sig[:3000]
+    sig = sig[sampRate*0:sampRate*1]  # read only first 1 second
 
-    # Converting signal into an analytic one using hilbert(), according to:
-    # https://github.com/jaidevd/pyhht/issues/43#issuecomment-398077924
-    sig = hilbert(sig)
+instf = []
 
-    instf, timestamps = inst_freq(sig)
+partLen = sampRate // 10  # divide into 100 ms parts
+imfs = []
+for i in range(len(sig)//partLen):
+    part = sig[i*partLen:(i+1)*partLen]
+    decomposer = EMD(part, n_imfs=8)
+    partImfs = decomposer.decompose()
 
-print('calc is over')
-# print(instf)
+    for j in range(len(partImfs) - 1):  # last element is residue
+        # Converting imfs into an analytic one using hilbert(), according to:
+        # https://github.com/jaidevd/pyhht/issues/43#issuecomment-398077924
+        partInstf, _ = inst_freq(hilbert(partImfs[j]))
+        if i == 0:
+            imfs.append([])
+            instf.append([])
+        imfs[j].extend(partImfs[j])
+        instf[j].extend(partInstf)
+
+    if i == 0:  # todo: remove whole if block after use
+        print(numpy.shape(partImfs))
+        plot_imfs(part, partImfs)
+
 print(numpy.shape(instf))
 print('plotting...')
-
-plt.plot(timestamps, sig[:len(timestamps)])
-plt.grid(True, linestyle='dotted')
-plt.xlabel('Time (Samples)')
-plt.ylabel('Amplitude')
-plt.title('Signal')
-plt.show()
-
-plt.plot(timestamps, instf)
-plt.grid(True, linestyle='dotted')
-plt.xlabel('Time (Samples)')
-plt.ylabel('Frequency')
-plt.title('Instantaneous frequency estimation')
-plt.show()
+for i in range(numpy.shape(instf)[0]):
+    plt.plot(range(len(sig)), sig, label="Orig", color='gray')
+    plt.plot(range(len(instf[i])), instf[i], label="Instf", color='green')
+    plt.plot(range(len(imfs[i])), imfs[i], label="Imf", color='orange')
+    plt.grid(True, linestyle='dotted')
+    plt.xlabel('Time (Samples)')
+    plt.ylabel('Amplitude')
+    plt.title('Instant Freq of IMF #' + str(i+1))
+    plt.legend()
+    plt.show()
