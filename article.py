b41ff3d18a2f891155e1e56fbb1bc311747f995f
atilileri <atil@live.it>
2019-02-11 01:04:27 +0300

    Breath Similarity Measure added. Makeup. Initial Classification done.

--- article.py
+++ article.py
@@ -12,11 +12,11 @@ path = utils.prefix + '/Initial Breath Examples/'
 # path = utils.prefix + '/METU Recordings/'
 
 '''
-II. INITIAL DETECTION ALGORITHM
+Section II. INITIAL DETECTION ALGORITHM
 '''
 
 '''
-A. Constructing the Template
+Section II-A : Constructing the Template
 '''
 
 allMatrixesOfExampleSet = list()
@@ -31,7 +31,7 @@ for fileInfo in exampleSet:
     # print(mfccMatrix)
     allMatrixesOfExampleSet.append(mfccMatrix)
 
-''' Step II-A.5:
+''' Section II-A.5:
 A mean cepstrogram is computed by averaging the matrices of the example set, as follows:
 T = 1/N * Epsilon( M(i) i=1,2,...,N )
 This defines the template matrix T. In a similar manner, a variance matrix V is computed, where the distribution of
@@ -40,7 +40,7 @@ each coefficient is measured along the example set.
 templateMatrix = np.mean(allMatrixesOfExampleSet, axis=0)
 varianceMatrix = np.var(allMatrixesOfExampleSet, axis=0)
 
-''' Step II-A.6:
+''' Section II-A.6:
 In addition to the template matrix, another feature vector is computed as follows: the matrices of the example set
 are concatenated into one matrix, and the singular value decomposition (SVD) of the resulting matrix is computed.
 Then, the normalized singular vector S corresponding to the largest singular value is derived. Due to the information
@@ -62,14 +62,24 @@ mainSingularVector = singularVectors[np.argmax(np.abs(singularValues))]
 # get normalized Singular Vector for calculations of Cn
 normSingVect = mainSingularVector / max(np.abs(mainSingularVector))
 
-'''
-B. Detection Phase
+print('Template & Variance Matrix, Singular Vector are Constructed')
+
+''' Section II-B : Detection Phase
 The input for the detection algorithm is an audio signal (a monophonic recording of either speech or song, with no
 background music), sampled with 44 kHz. The signal is divided into consecutive analysis frames (with a hop size of 
 10 ms). For each frame, the following parameters are computed: the cepstrogram (MFCC matrix, see Fig. 4), short-time
 energy, zero-crossing rate, and spectral slope (see below). Each of these is computed over a window located around the
 center of the frame.
 '''
+
+# Go over example set again and find params for Breath Similarity Measurements in "Step II-C.Detection"
+bsmArray = []
+for cepsEach in allMatrixesOfExampleSet:
+    bsmEach = utils.calcBSM(cepsEach, templateMatrix, varianceMatrix, normSingVect)
+    bsmArray.append(bsmEach)
+bm = min(bsmArray)
+bsmThreshold = bm / 2
+
 # Read input audio signal
 # Note that this file can not be added to github, because it is larger than the size limit(100MB)
 path = './whole_speech.wav'
@@ -78,29 +88,32 @@ fs, inputSignal = utils.readMonoWav(path)
 # print('Input File:', path)
 # print(fs, 'Hz, Size=', inputSignal.dtype, '*', len(inputSignal), 'bytes, Array:', inputSignal)
 
-''' Step II-B.1:
-The MFCC matrix is computed as in the template generation process (see previous section). For this purpose, the
-length of the MFCC analysis window used for the detection phase must match the length of the frame(frameLengthInSecs)
-derived from each breath example in the training phase.
-'''
 # todo - ai: change hop size to 0.010, now its 10 seconds for debugging purposes
 hopSize = 10  # hop size is 10 ms for detection phase
-windowLengthInSamples = int(0.010 * fs)  # window size is 10 ms for detection phase
 for i in range(0, len(inputSignal), int(hopSize * fs)):
+    # MyNote 1 - ai : Since there is no explicit information on the length of each consecutive analysis frame in
+    #  detection phase, a minimum value is taken as analysis frame size. Which is the length of the frame
+    #  (frameLengthInSecs) derived from each breath example in the training phase.
+    # todo - hh : ask if frameLengthInSecs | there is a general frame length in the literature?
     # Index to stop: At the end of the analysis frame, index must stop before the file ends.
     # This assignment adjusts the last step size according to info above.
-    # print(len(inputSignal))
-    # print(i + int(frameLengthInSecs * fs))
     stopIdx = min(i + int(frameLengthInSecs * fs), len(inputSignal) - 1)
-
     analysisFrame = inputSignal[i:stopIdx]
-    centerWindow = utils.getCenterWindow(analysisFrame, windowLengthInSamples)
 
-    # todo - ai : createMfccMatrix() should work with centerWindow, not analysisFrame. Throws error now. Investigate!
+    ''' Section II-B.1:
+    The MFCC matrix is computed as in the template generation process (see previous section). For this purpose, the
+    length of the MFCC analysis window used for the detection phase must match the length of the frame 
+    (frameLengthInSecs) derived from each breath example in the training phase.
+    '''
     # The Cepstrogram (MFCC matrix) is computed over a window located around the center of the frame
-    cepstogramXi = utils.createMfccMatrix(analysisFrame, fs)
-    # print(np.shape(mfccMatrix))
-    ''' Step II-B.2:
+    windowLengthInSamples = len(analysisFrame)  # window size is frameLengthInSecs for mfcc calculations
+    centerWindow = utils.getCenterWindow(analysisFrame, windowLengthInSamples)
+    # Since we are using frameLengthInSecs as the analysis frame length(Refer to "Note 1" above), we do not need to
+    # get the center window of the frame with its own length :) but, we are doing it for generality of the code.
+    # This decision makes sense when frame length in Detection Phase is changed to a value other than MFCC analysis
+    # frame length. Note 1 states this also. But for now, centerWindow is exactly same as analysisFrame in below :)
+    cepstogramXi = utils.createMfccMatrix(centerWindow, fs)
+    ''' Section II-B.2:
     The short-time energy is computed according to the following:
     E = 1/N * Epsilon(goes n=[N0, N0+(N-1)])(x^2[n])
     where x[n] is the sampled audio signal, and N is the window length in samples (corresponding to 10 ms). It is
@@ -108,81 +121,65 @@ for i in range(0, len(inputSignal), int(hopSize * fs)):
     E, dB = 10 * log10(E)
     '''
     # Short Time Energy is computed over a window located around the center of the frame
+    windowLengthInSamples = int(0.010 * fs)  # window size is 10 ms for STE calculations
+    centerWindow = utils.getCenterWindow(analysisFrame, windowLengthInSamples)
     steXi, db = utils.calcShortTimeEnergy(centerWindow)
-    ''' Step II-B.3:
-    The zero-crossing rate (ZCR) is defined as the number of times the audio waveform changes its sign, normalized by
-    the window length N in samples (corresponding to 10 ms)
+    ''' Section II-B.3:
+    The zero-crossing rate (ZCR) is defined as the number of times the audio waveform changes its sign, normalized
+    by the window length N in samples (corresponding to 10 ms)
     ZCR = 1/N * Epsilon(goes n=[N0+1, N0+(N-1)])( 0.5 * abs( sign(x[n]) - sign(x[n-1]) ) )
     '''
     # Zero Crossing Rate is computed over a window located around the center of the frame
+    windowLengthInSamples = int(0.010 * fs)  # window size is 10 ms for ZCR calculations
+    centerWindow = utils.getCenterWindow(analysisFrame, windowLengthInSamples)
     zcrXi = utils.calcZeroCrossingRate(centerWindow)
-    ''' Step II-B.4:
+    ''' Section II-B.4:
     The spectral slope is computed by taking the discrete Fourier transform of the analysis window, evaluating its
     magnitude at frequencies of pi/2 and pi (corresponding here to 11 and 22 kHz, respectively), and computing the
     slope of the straight line fit between these two points. It is known that in voiced speech most of the spectral
-    energy is contained in the lower frequencies (below 4 kHz). Therefore, in voiced speech, the spectrum is expected to 
-    be rather flat between 11 and 22 kHz. In periods of silence, the waveform is close to random, which also leads to a
-    relatively flat spectrum throughout the entire band. This suggests that the spectral slope in voiced/silence parts
-    would yield low values, when measured as described previously. On the other hand, in breath sounds, like in most 
-    unvoiced phonemes, there is still a significant amount of energy in the middle frequency band (10–15 kHz) and
-    relatively low energy in the high band (22 kHz). Thus, the spectral slope is expected to be steeper, and could be
-    used to differentiate between voiced/silence and unvoiced/breath. As such, the spectral slope is used here as an
-    additional parameter for identifying the edges of the breath (see Section III).
+    energy is contained in the lower frequencies (below 4 kHz). Therefore, in voiced speech, the spectrum is
+    expected to be rather flat between 11 and 22 kHz. In periods of silence, the waveform is close to random, which
+    also leads to a relatively flat spectrum throughout the entire band. This suggests that the spectral slope in
+    voiced/silence parts would yield low values, when measured as described previously. On the other hand, in breath
+    sounds, like in most unvoiced phonemes, there is still a significant amount of energy in the middle frequency 
+    band (10–15 kHz) and relatively low energy in the high band (22 kHz). Thus, the spectral slope is expected to be
+    steeper, and could be used to differentiate between voiced/silence and unvoiced/breath. As such, the spectral
+    slope is used here as an additional parameter for identifying the edges of the breath (see Section III).
     '''
+    # The Spectral Slope is computed over a window located around the center of the frame
+    # MyNote 2 - ai : window length is not referred exactly in the article so, whole frame is taken, same as mfcc.
+    # todo - hh : ask if 10 ms | mfcc analysis window length | full analysis frame ? (last 2 are same for now)
+    windowLengthInSamples = len(analysisFrame)  # window size is frameLengthInSecs for slope calculations
+    centerWindow = utils.getCenterWindow(analysisFrame, windowLengthInSamples)
     slopeXi = utils.calcSpectralSlope(centerWindow, fs)
-
+    
+    ''' Section II-C : Computation of the Breath Similarity Measure
+    Once the aforementioned parameters are computed for a given frame Xi, its short-time cepstrogram (MFCC matrix)
+    is used for calculating its breath similarity measure. The similarity measure, denoted B(Xi, T, V, S), is 
+    computed between the cepstrogram of the frame, M(Xi), the template cepstrogram T (with V being the variance
+    matrix) and the singular vector S . The steps of the computation are as follows (Fig. 6):
     '''
-    C.Computation of the Breath Similarity Measure
-    Once the aforementioned parameters are computed for a given frame Xi, its short-time cepstrogram (MFCC matrix) is
-    used for calculating its breath similarity measure. The similarity measure, denoted B(Xi, T, V, S), is computed between 
-    the cepstrogram of the frame, M(Xi), the template cepstrogram T (with V being the variance matrix) and the singular 
-    vector S . The steps of the computation are as follows (Fig. 6):
-    '''
-    ''' Step II-C.1:
-    The normalized difference matrix  D = (M(Xi) - T) / V  is computed. The normalization (element-by-element) by the 
-    variance matrix is performed in order to compensate for the differences in the distributions of the various cepstral
-    coefficients.
-    '''
-    diffXi = np.subtract(cepstogramXi, templateMatrix)
-    normDiffXi = np.divide(diffXi, varianceMatrix)
+    bsmXi = utils.calcBSM(cepstogramXi, templateMatrix, varianceMatrix, normSingVect)
 
-    # Reminder Here: The cepstrogram is defined as a matrix whose columns are the MFCC vectors for each subframe.
-    ''' Step II-C.2:
-    The difference matrix is liftered by multiplying each column with a half-Hamming window that emphasizes the lower
-    cepstral coefficients. It has been found in preliminary experiments, that this procedure yields better separation
-    between breath sounds and other sounds (see also [2]).
+    ''' Section II-C.Detection
+    The breath detection involves a two-step decision. The initial decision treats each frame independently of other
+    frames and classifies each as breathy/not breathy based on its similarity measure B(Xi, T, V, S), energy and
+    zero-crossing rate. A frame is initially classified as breathy if all three of the following occur:
+    1) The breath similarity measure is above a given threshold. This threshold is initially set in the learning phase,
+    during the template construction, when the breath similarity measure B(Xi, T, V, S) is computed for each of the
+    examples. The minimum value of the similarity measures between each of the examples and the template is determined, 
+    denoted by Bm. The threshold is set to Bm / 2. The logic behind this setting is that the frame-to-template
+    similarity of breath sounds in general is expected to be somewhat lower than the similarity among examples used to
+    construct the template in the first place. This parameter is referred as "bsmThreshold".
+    2) The energy is below a given threshold, which is chosen to be below the average energy of voiced speech (see
+    Section III-A). This parameter is referred as "engThreshold".
+    3) The zero-crossing rate is below a given threshold. Experimental data have shown that ZCR above 0.25 (assuming
+    a sampling rate of 44 kHz) is exhibited only by a number of unvoiced fricatives, and breath sounds have much lower
+    ZCR (see Section III-A). This parameter is referred as "zcrThreshold".
     '''
-    lifter = np.hamming(26)[13:]  # get half-Hamming window
-    liftNormDiffXi = np.multiply(normDiffXi, lifter)
-
-    ''' Step II-C.3:
-    A first similarity measure(Cp) is computed by taking the inverse of the sum of squares of all elements of the
-    normalized difference matrix, according to the following equation:
-        Cp = 1 / Epsilon(goes i=[1,n], j=[1, Ne])(Dij^2)
-    where n is the number of subframes, and Ne is the number of MFC coefficients computed for each subframe.
-    When the cepstrogram is very similar to the template, the elements of the difference matrix should be small, leading
-    to a high value of this similarity measure. When the frame contains a signal which is very different from breath,
-    the measure is expected to yield small values. This template matching procedure with a scaled Euclidean distance is
-    essentially a special case of a two-class Gaussian classifier [30] with a diagonal covariance matrix. This is due to
-    the computation of the MFCC, which involves a discrete cosine transform as its last step [20], known for its
-    tendency to decorrelate the mel-scale filter log-energies [22].
-    '''
-    squaresXi = np.square(liftNormDiffXi)
-    sumOfSquaresXi = np.sum(squaresXi)
-    cp = 1 / sumOfSquaresXi
-
-    ''' Step II-C.4:
-    A second similarity measure(Cn) is computed by taking the sum of the inner products between the singular vector
-    (see Section II-A) and the normalized columns of the cepstrogram. Since the singular vector is assumed to capture
-    the important characteristics of breath sounds, these inner products (and, therefore, Cn) are expected to be small
-    when the frame contains information from other phonemes.
-    '''
-    innerProducts = []
-    for col in cepstogramXi:
-        normCol = col / np.max(np.abs(col))
-        innerProduct = np.inner(normCol, normSingVect)
-        innerProducts.append(innerProduct)
-
-    cn = np.sum(innerProducts)
-
-    print('Similarity Measure for frame', i, ':', cp * cn)
+    zcrThreshold = 0.25
+    engThreshold = 999999  # todo - ai : calculate this. Pseudo for now
+    if bsmXi > bsmThreshold and zcrXi < zcrThreshold and steXi < engThreshold:
+        print(i, '-', stopIdx, ': BREATH')
+    else:
+        print(i, '-', stopIdx, ': no')
--- utils.py
+++ utils.py
@@ -25,15 +25,16 @@ else:
 # function to read wav files, assuring that resulting signal is mono channel.
 def readMonoWav(pathToFile):
     samplingRate, samples = scipy.io.wavfile.read(pathToFile)
-    # stereo to mono
+    # Convert multi channel record to mono
     if len(samples.shape) > 1 and 1 < samples.shape[1]:  # python evals expressions LTR so it's safe to write this
-        # maybe all channels can be used as different inputs to augment the data. Now using only first channel
-        # Sample mean convert code
+        # Solution 1: Convert to mono by taking mean of the channels
         # samplesMono = np.mean(samples, axis=1, dtype=samples.dtype)
-        # Sample first channel code
+        # Solution 2: Convert to mono by taking only first channel
         samplesMono = samples[:, 0]
+        # todo - ai : all channels can be used as different inputs to augment the data? For now, get first channel only.
     else:
         samplesMono = samples
+    # print('File: \'', pathToFile, '\', Length: ', len(samplesMono)/samplingRate, 'seconds.')
     return samplingRate, samplesMono
 
 
@@ -43,7 +44,7 @@ def readExampleSet(folderPath):
     minlen = 999999  # pseudo big number
     sampleRate = 0
     '''
-    Step II-A.1:
+    Section II-A.1:
     Several signals containing isolated breath examples are selected, forming the example set. From each example, a section
     of fixed length, typically equal to the length of the shortest example in the set (about 100–160 ms), is derived.
     This length is used throughout the algorithm as the frame length (see Section II-C).
@@ -57,7 +58,7 @@ def readExampleSet(folderPath):
             # todo - ai: minlen diff looks huge, create better samples
             # print('min frame:', minlen)
 
-    # fix frame length. Refer to Step B.1
+    # fix frame length. Refer to Section B.1
     for file in exampleSet:
         file[2] = file[2][:minlen]
 
@@ -72,7 +73,7 @@ def createMfccMatrix(sig,
     subframeDurationBySample = int(subframeDuration * sampRate)
     alpha = 0.95
 
-    ''' Step II-A.2:
+    ''' Section II-A.2:
     Each breath example is divided into short consecutive subframes, with duration of 10 ms and hop size of 5 ms.
     Each subframe is then pre-emphasized using a first-order difference filter ( H(z) = 1 - alpha * z^-1 
     where alpha = 0.95)
@@ -86,7 +87,7 @@ def createMfccMatrix(sig,
         # print(stopIdx)
         emphasized = signal.lfilter([1, -1 * alpha], [1., 1.], sig[i:stopIdx])
 
-        ''' Step II-A.3:
+        ''' Section II-A.3:
         For each breath example, the MFCC are computed for every subframe, thus forming a short-time cepstrogram
         representation of the example. The cepstrogram is defined as a matrix whose columns are the MFCC vectors
         for each subframe. Each such matrix is denoted by M(i), i=1,2,...,N where N is the number of examples in the
@@ -94,7 +95,7 @@ def createMfccMatrix(sig,
         '''
         # http://python-speech-features.readthedocs.io/en/latest/#python_speech_features.base.mfcc
         # mfcc() function is designed to work on longer signals and divide them into subframes using winlen and winstep
-        # parameters. What we do instead is, we split the sgnal ourselves into subframes, apply the filter on the
+        # parameters. What we do instead is, we split the signal ourselves into subframes, apply the filter on the
         # subframe and put it into mfcc() function. So we get 1 mfcc row for each subframe. Because function is designed
         # to work on longer signals, it returns 2d array, each row holding 1 mfcc vector. Since we only get 1 vector,
         # our result is 2d array with the shape of (1,13). So we only get 1st row, as 1d array of 13 elements.
@@ -105,7 +106,7 @@ def createMfccMatrix(sig,
         # print(len(mfccMatrix))
         # print(mfccMatrix)
 
-    ''' Step II-A.4:
+    ''' Section II-A.4:
     For each column of the cepstrogram, DC removal is performed, resulting in the matrix M(i) i=1,2,...,N.
     '''
     # print('Before:', mfccMatrix)
@@ -120,14 +121,12 @@ def createMfccMatrix(sig,
 
 
 def getCenterWindow(frame, windowLength):
-    midIndex = len(frame) // 2
-    halfWindowLen = windowLength / 2
-    if midIndex >= halfWindowLen:
-        # we cast to int below, instead of above(where halfWindowLen is defined)
-        # because otherwise our slice is one element short, for odd windowLength values
-        centerWindow = frame[int(midIndex-halfWindowLen):int(midIndex+halfWindowLen)]
-    else:
-        centerWindow = None
+    midIndex = len(frame) / 2
+    halfWindowLen = min(midIndex, windowLength / 2)
+    # we cast to int below, instead of above(where halfWindowLen is defined)
+    # because otherwise our slice is one element short, for odd windowLength values
+    centerWindow = frame[int(midIndex-halfWindowLen):int(midIndex+halfWindowLen)]
+
     return centerWindow
 
 
@@ -138,6 +137,8 @@ def calcShortTimeEnergy(window):
     where x[n] is the sampled audio signal, and N is the window length in samples (corresponding to 10 ms). It is
     then converted to a logarithmic scale
     E, dB = 10 * log10(E)
+    :param window: given window
+    :return: short time energy of the window
     """
     squared = np.square(window, dtype='int64')
     summed = np.sum(squared, dtype='int64')
@@ -149,9 +150,11 @@ def calcShortTimeEnergy(window):
 
 def calcZeroCrossingRate(window):
     """ calculates zero-crossing rate of a given window
-    The zero-crossing rate (ZCR) is defined as the number of times the audio waveform changes its sign, normalized by
+     The zero-crossing rate (ZCR) is defined as the number of times the audio waveform changes its sign, normalized by
     the window length N in samples (corresponding to 10 ms)
     ZCR = 1/N * Epsilon(goes n=[N0+1, N0+(N-1)])( 0.5 * abs( sign(x[n]) - sign(x[n-1]) ) )
+    :param window: given window
+    :return: zero crossing rate of the window
     """
     zeroCrossings = 0
     for i in range(1, len(window)):
@@ -164,6 +167,9 @@ def calcSpectralSlope(window, fs):
     The spectral slope is computed by taking the discrete Fourier transform of the analysis window, evaluating its
     magnitude at frequencies of pi/2 and pi (corresponding here to 11 and 22 kHz, respectively), and computing the
     slope of the straight line fit between these two points.
+    :param window: given window
+    :param fs: sampling rate
+    :return: spectral slope of the window
     """
     # todo - hh: ask if implementation is right
     magnitudes = np.abs(np.fft.fft(window))
@@ -177,3 +183,72 @@ def calcSpectralSlope(window, fs):
     x2 = frequencies[indexPi]
     # todo - ai: I added 10k factor below to make number compareable. Check for future
     return np.abs((y2-y1) / ((x2-x1)/10000))
+
+
+def calcBSM(cepstogram, templateMatrix, varianceMatrix, normSingVect):
+    """ This function calculates Breath Similarity Measure for given frame
+    :param cepstogram: cepstogram of the frame (also reffered as M)
+    :param templateMatrix: the template cepstogram (also reffered as T)
+    :param varianceMatrix: the variance matrix (also reffered as V)
+    :param normSingVect: the singular vector, normalized (also reffered as S)
+    :return: bsm - Breath Similarity Measure of the frame
+    """
+
+    ''' Section II-C.1:
+    The normalized difference matrix  D = (M(Xi) - T) / V  is computed. The normalization (element-by-element) by the 
+    variance matrix is performed in order to compensate for the differences in the distributions of the various cepstral
+    coefficients.
+    '''
+    diffXi = np.subtract(cepstogram, templateMatrix)
+    normDiffXi = np.divide(diffXi, varianceMatrix)
+
+    # Reminder Here: The cepstrogram is defined as a matrix whose columns are the MFCC vectors for each subframe.
+    ''' Section II-C.2:
+    The difference matrix is liftered by multiplying each column with a half-Hamming window that emphasizes the lower
+    cepstral coefficients. It has been found in preliminary experiments, that this procedure yields better separation
+    between breath sounds and other sounds (see also [2]).
+    '''
+    lifter = np.hamming(26)[13:]  # get half-Hamming window
+    liftNormDiffXi = np.multiply(normDiffXi, lifter)
+
+    ''' Section II-C.3:
+    A first similarity measure(Cp) is computed by taking the inverse of the sum of squares of all elements of the
+    normalized difference matrix, according to the following equation:
+        Cp = 1 / Epsilon(goes i=[1,n], j=[1, Ne])(Dij^2)
+    where n is the number of subframes, and Ne is the number of MFC coefficients computed for each subframe.
+    When the cepstrogram is very similar to the template, the elements of the difference matrix should be small, leading
+    to a high value of this similarity measure. When the frame contains a signal which is very different from breath,
+    the measure is expected to yield small values. This template matching procedure with a scaled Euclidean distance is
+    essentially a special case of a two-class Gaussian classifier [30] with a diagonal covariance matrix. This is due to
+    the computation of the MFCC, which involves a discrete cosine transform as its last step [20], known for its
+    tendency to decorrelate the mel-scale filter log-energies [22].
+    '''
+    squaresXi = np.square(liftNormDiffXi)
+    sumOfSquaresXi = np.sum(squaresXi)
+    cp = 1 / sumOfSquaresXi
+
+    ''' Section II-C.4:
+    A second similarity measure(Cn) is computed by taking the sum of the inner products between the singular vector
+    (see Section II-A) and the normalized columns of the cepstrogram. Since the singular vector is assumed to capture
+    the important characteristics of breath sounds, these inner products (and, therefore, Cn) are expected to be small
+    when the frame contains information from other phonemes.
+    '''
+    innerProducts = []
+    for col in cepstogram:
+        normCol = col / np.max(np.abs(col))
+        innerProduct = np.inner(normCol, normSingVect)
+        innerProducts.append(innerProduct)
+
+    cn = np.sum(innerProducts)
+
+    ''' Section II-C.5:
+    The final breath similarity measure is defined as the product of the two measures: . It was found experimentally
+    that this combination of similarity measures yields better separation between breath and nonbreath than using
+    just the difference matrix or the singular vector.
+    '''
+    # todo - hh : there are negative values. But in Section TBD, there is a threshold value to pass for detection. How
+    #  negative values can pass this threshold. There is a 'reciprocal of breath similarity function' statement in
+    #  figure 5
+    bsm = cp * cn
+    # print('Similarity Measure for frame', i, ':', bsm)
+    return bsm
