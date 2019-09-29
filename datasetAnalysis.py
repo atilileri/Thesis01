dbf003e65cd20f4e8057de3b02eb948f9858b3b2
atilileri <atil@live.it>
2019-09-29 07:39:56 +0300

    analysis for new filename format fix

--- datasetAnalysis.py
+++ datasetAnalysis.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-filepath = 'D:/atili/MMIExt/Audacity/METU Recordings/Dataset/max_sample_set/wav/'
+filepath = 'D:/atili/MMIExt/Audacity/METU Recordings/Dataset/Inputs_Max/wav/'
 
 lengths = list()
 for root, directories, files in os.walk(filepath):
@@ -10,16 +10,14 @@ for root, directories, files in os.walk(filepath):
         if '.wav' in file:
             # print('Extracting Breaths of:', file, 'at', root)
             length = file.split('.')[2]
-            while len(length) < 3:
-                length += '0'
-            length = int(length)
+            length = int(length)/10
             lengths.append(length)
             print(file, length)
 
 print(max(lengths))
-stats = np.zeros(max(lengths)+1)
+stats = np.zeros(int(max(lengths))+1)
 for l in lengths:
-    stats[l] += 1
+    stats[int(l)] += 1
 
 # plt.scatter(range(len(stats)), stats)
 plt.plot(stats, linewidth=0.8)
@@ -27,10 +25,10 @@ plt.show()
 
 step = 50
 start = 200
-for minLen in range(start, max(lengths), step):
+for minLen in range(start, int(np.ceil(max(lengths))), step):
     maxLen = minLen+step
     print(minLen, '-', maxLen, ':', sum(minLen <= j < maxLen for j in lengths) / len(lengths))
-print('200 <= i < 500 :', sum(200 <= i < 500 for i in lengths) / len(lengths))
-print('200 <= i < 550 :', sum(200 <= i < 550 for i in lengths) / len(lengths))
-print('200 <= i < 600 :', sum(200 <= i < 600 for i in lengths) / len(lengths))
-print('200 <= i < 650 :', sum(200 <= i < 650 for i in lengths) / len(lengths))
+print('200ms <= i < 500ms :', sum(200 <= i < 500 for i in lengths) / len(lengths))
+print('200ms <= i < 550ms :', sum(200 <= i < 550 for i in lengths) / len(lengths))
+print('200ms <= i < 600ms :', sum(200 <= i < 600 for i in lengths) / len(lengths))
+print('200ms <= i < 650ms :', sum(200 <= i < 650 for i in lengths) / len(lengths))
